b7cd20db03c55305a1de7b76de7d54a34d72813d
Elad Gelerenter <eladg3@gmail.com>
2024-06-27 19:26:51 +0300

    Add unit tests

---
+++ tests/Array.spec.js
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+
+describe("Stam tests suite", () => {
+    describe("Test array get sorted", () => {
+        it("should return sorted array", () => {
+            const arr = [5, 3, 8, 1, 2];
+            expect(arr.sort).to.be.eql([1, 2, 3, 5, 8]);
+        })
+    })
+})
