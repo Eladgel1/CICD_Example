72239e05ddaa7fdfcadff51ba78d76bddef88a8e
Elad Gelerenter <eladg3@gmail.com>
2024-06-27 20:20:01 +0300

    Fix syntax

--- tests/Array.spec.js
+++ tests/Array.spec.js
@@ -4,7 +4,7 @@ describe("Stam tests suite", () => {
     describe("Test array get sorted", () => {
         it("should return sorted array", () => {
             const arr = [5, 3, 8, 1, 2];
-            expect(arr.sort).to.be.eql([1, 2, 3, 5, 8]);
+            expect(arr.sort()).to.be.eql([1, 2, 3, 5, 8]);
         })
     })
 })
