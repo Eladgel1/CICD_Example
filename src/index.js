aef69b3111f7867fa934d4aac100b9da27466a38
Elad Gelerenter <eladg3@gmail.com>
2024-06-27 19:21:38 +0300

    Add minimal express server

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req,res) => {
+    const response = '{"text": "Hello from server"}';
+
+    res.send(response);
+});
+
+app.listen(port, () =>{
+    console.log('Server is running on port' + port);
+});
+
