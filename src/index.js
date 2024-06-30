3f911fa6c65c32aefb35023f322e94d543ad0cc6
Elad Gelerenter <eladg3@gmail.com>
2024-06-30 20:18:13 +0300

    Add names and endpoint

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req,res) => {
     const response = '{"text": "Hello from server"}';
 
     res.send(response);
+});
+
+app.post('/names', (req,res) => {
+    const names = req.body;
+
+    console.log("Got new post message:" ,JSON.stringify(names));
+
+    res.json({message: "Hey I got your message!", names: names});
 });
 
 app.listen(port, () =>{
